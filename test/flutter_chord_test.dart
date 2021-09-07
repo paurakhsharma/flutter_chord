4a7f4a6fbc8d677c8cce23bb7b139bed128c65b4
Paurakh Sharma Humagain <paurakh011@gmail.com>
2021-09-07 19:23:23 +0545

    Add test

--- test/flutter_chord_test.dart
+++ test/flutter_chord_test.dart
@@ -1,8 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chord/flutter_chord.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-
 void main() {
-  test('adds one to input values', () {
-    // TODO: Write tests...
+  testWidgets('Can parse single line chord', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        builder: (context, _) {
+          String text = '[C]This is t[D]he lyrics[E]';
+          final textStyle = TextStyle(fontSize: 18, color: Colors.green);
+
+          final processor = ChordProcessor(context);
+          final chordDocument = processor.processText(
+            text: text,
+            lyricsStyle: textStyle,
+            chordStyle: textStyle,
+          );
+
+          expect(
+            chordDocument.chordLyricsLines.length,
+            1,
+          );
+          expect(
+            chordDocument.chordLyricsLines.first.chords.length,
+            3,
+          );
+          expect(
+            chordDocument.chordLyricsLines.first.chords.first.chordText,
+            'C',
+          );
+          expect(
+            chordDocument.chordLyricsLines.first.chords.first.leadingSpace,
+            0.0,
+          );
+
+          expect(
+            chordDocument.chordLyricsLines.first.chords[1].chordText,
+            'D',
+          );
+
+          final textWidth = processor.textWidth('This is t', textStyle);
+          final chordWidth = processor.textWidth('C', textStyle);
+          expect(
+            chordDocument.chordLyricsLines.first.chords[1].leadingSpace,
+            textWidth - chordWidth,
+          );
+
+          // The builder function must return a widget.
+          return Container();
+        },
+      ),
+    );
   });
 }
