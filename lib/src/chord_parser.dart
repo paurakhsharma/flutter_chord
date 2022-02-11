6f35fb5289da30e4b498ecb3db7be254fd2d30f3
auhsor <auhsor@gmail.com>
2022-02-11 15:39:54 +1100

    Update chord_parser.dart

--- lib/src/chord_parser.dart
+++ lib/src/chord_parser.dart
@@ -13,9 +13,65 @@ class ChordProcessor {
     required String text,
     required TextStyle lyricsStyle,
     required chordStyle,
+    required widgetPadding,
     int transposeIncrement = 0,
   }) {
     final lines = text.split('\n');
+
+    bool _chordHasStartedOuter = false;
+    String _currentCharacters = '';
+    int _lastSpace = 0;
+    String _currentLine = '';
+    String _character = '';
+    double _media = MediaQuery.of(context).size.width;
+    int _characterIndex = 0;
+
+    //list to store our updated lines without overflows
+    final _newLines = <String>[];
+
+    //loop through the lines
+    for (var i = 0; i < lines.length; i++) {
+      _currentLine = lines[i];
+
+      //check if we have a long line
+      if (textWidth(_currentLine, lyricsStyle) >= _media) {
+        
+        //work our way through the line and split when we need to
+        for (var j = 0; j < _currentLine.length; j++) {
+          _character = _currentLine[j];
+          if (_character == '[')
+            _chordHasStartedOuter = true;
+          else if (_character == ']')
+            _chordHasStartedOuter = false;
+          else if (_chordHasStartedOuter == true)
+            ;
+          else {
+            _currentCharacters += _character;
+            if (_character == ' ') {
+              //use this marker to only split where there are spaces
+              _lastSpace = j;
+            }
+
+            //This is the point where we need to split
+            //I've added widgetPadding as a parameter to be passed from the build function
+            //It is intended to allow for padding in the widget when comparing it to screen width
+            //I had to add a little extra (about 10) to stop overflow.
+            if (textWidth(_currentCharacters, lyricsStyle) + widgetPadding >= _media) {
+              _newLines.add(lines[i].substring(_characterIndex, _lastSpace).trim());
+              _currentCharacters = '';
+              _characterIndex = _lastSpace;
+            }
+          }
+        }
+        //add the rest of the long line
+        _newLines.add(lines[i].substring(_characterIndex, lines[i].length).trim());
+        ;
+      } else {
+        //otherwise just add the regular line
+        _newLines.add(lines[i].trim());
+      }
+    }
+    ;
     List<ChordLyricsLine> _chordLyricsLines =
         lines.map<ChordLyricsLine>((line) {
       ChordLyricsLine _chordLyricsLine = ChordLyricsLine([], '');
