0742adabb9366053f73515697e14ba3e0a89e670
paurakhsharma <paurakh011@gmail.com>
2026-07-09 10:16:13 +0545

    Add chord transposer export
    - Introduced export for chord_transposer.dart in flutter_chord.dart to enhance chord manipulation capabilities.

--- lib/flutter_chord.dart
+++ lib/flutter_chord.dart
@@ -2,6 +2,7 @@ library flutter_chord;
 
 export 'src/chord_parser.dart';
 export 'src/chord_position.dart';
+export 'src/chord_transposer.dart';
 export 'src/lyrics_renderer.dart';
 export 'src/model/chord_lyrics_document.dart';
 export 'src/model/chord_lyrics_line.dart';
